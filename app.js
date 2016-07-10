ff0fb9a082522ab4a873c1fb608de68bc158847b
Ted Tzuridis <tzuridis@gmail.com>
2016-07-09 23:33:34 -0400

    finished

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-function fizzbuzz () {
-	for (i = 0; i > 100; i++) {
-		if (i % 3 === 0){
-			'Fizz'
-		} else {
-			if (i % 5 === 0) {
-				'Buzz'
+
+	for (i = 1; i < 101; i++) {
+		if (i % 15 === 0){
+			document.write('FizzBuzz');
+			document.write('<br/>');
+		} 
+			else if (i % 3 === 0) {
+				document.write('Fizz');
+				document.write('<br/>');
 			}
+			else if (i % 5 === 0) {
+			document.write('Buzz');
+			document.write('<br/>');
+		} else {
+			document.write(i)
+			document.write('<br/>');
 		}
 	}
-}
+	
