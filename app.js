45feafd64f58d06cd30b7fcdf21e00502d0a4e28
Ted Tzuridis <tzuridis@gmail.com>
2016-07-11 02:30:29 -0400

    Finished refactor

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
+var supply = window.prompt('Supply an integer:');
+if (supply !== Number) {
+    parseInt(supply);
+    console.log(fizzbuzz(supply));
+} else if (supply === String) {
+    document.write('Unable to comply, value is not a number!')
+}
 
-	for (i = 1; i < 101; i++) {
-		if (i % 15 === 0){
-			document.write('FizzBuzz');
-			document.write('<br/>');
-		} 
-			else if (i % 3 === 0) {
-				document.write('Fizz');
-				document.write('<br/>');
-			}
-			else if (i % 5 === 0) {
-			document.write('Buzz');
-			document.write('<br/>');
-		} else {
-			document.write(i)
-			document.write('<br/>');
-		}
-	}
-	
+
+function fizzbuzz(supply) {
+    for (i = 1; i < supply; i++) {
+        if (i % 15 === 0) {
+            document.write('FizzBuzz');
+            document.write('<br/>');
+        } else if (i % 3 === 0) {
+            document.write('Fizz');
+            document.write('<br/>');
+        } else if (i % 5 === 0) {
+            document.write('Buzz');
+            document.write('<br/>');
+        } else {
+            document.write(i);
+            document.write('<br/>');
+        }
+    }
+}
